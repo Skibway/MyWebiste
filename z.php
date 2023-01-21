e20fbead4a78dde84e0530c323d1d7383b16c71c
Skibway <86574686+Skibway@users.noreply.github.com>
2023-01-21 21:13:42 +0100

    Entrance

---
+++ z.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html >
+
+<html>
+
+<head>
+
+<title>baza danych SPI</title>
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+  <meta http-equiv="content-language" content="pl">
+  <meta name ="description" content="opis zawartosci strony">
+  <meta name="author" content="dane autora">
+
+
+
+</head>
+<link rel="stylesheet" href="Style/arkusz.css"/>
+<body>
+
+<table width="960" border="1" align="center">
+  <tr>
+    <td colspan="2"  height="230">headlines</td>
+  </tr>
+  <tr>
+    <td width="200" height="400" valign="top">
+		<table cellspacing="0"  width="200" border="1">
+    <tr>
+      <td >Strona główna</td>
+    </tr>
+    <tr>
+      <td>Lista studentów</td>
+    </tr>
+    <tr>
+      <td>Dodaj studenta</td>
+    </tr>
+    <tr>
+      <td>Edytuj studenta</td>
+    </tr>
+    <tr>
+      <td>Usuń studenta</td>
+    </tr>
+    <tr>
+     <td>Search</td>
+    </tr>
+   
+
+  </table>
+		</td>
+		<td>
+
+<p>
+content
+<?PHP
+  echo "Skrypt napisany w PHP";
+  echo "br/";
+  $liczba1=12;
+  $liczba2=103;
+  $suma=$liczba1+$liczba2;
+  echo "<b>Suma podanych liczb wynosi: </b> ";
+  echo $suma;
+  echo "<br/>"
+?>
+</p>
+		</td>
+  </tr>
+  <tr>
+    <td colspan="2" height="50">stopka</td>
+  </tr>
+
+</table>
+</body>
+</html>
