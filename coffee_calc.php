c97870d0e5eb566b4236005444bac6b1a7556626
Skibway <86574686+Skibway@users.noreply.github.com>
2023-01-22 23:28:20 +0100

    Update

---
+++ coffee_calc.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+<title>C8H10N4O2 * ☕️ * </title>
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+  <meta http-equiv="content-language" content="pl">
+  <meta name ="description" content="opis zawartosci strony">
+  <meta name="author" content="dane autora">
+
+
+
+</head>
+<link rel="stylesheet" href="Style/arkusz.css" />
+<body class="body_mod">
+<table width="960" border="1" align="center" class="main_tbl">
+  <tr>
+    <td colspan="2" height="200">
+      <figure>
+        <img src="images/ziarna.png">
+      </figure>
+    </td>
+  </tr>
+  <tr>
+    <td width="200" height="200" valign="top">
+	<table cellspacing="0"  width="200" border="1" class="panel_tbl" >
+    <tr>
+      <td class="panel_tbl_single">
+        <button class="btn">Strona główna</button>
+      </td>
+    </tr>
+    <tr>
+      <td class="panel_tbl_single">
+        <button class="btn">Rodzaje kawy</button>
+      </td>
+    </tr>
+    <tr>
+      <td class="panel_tbl_single">
+        <button class="btn">Zamów kawę</button>
+      </td>
+    </tr>
+    <tr>
+      <td  class="panel_tbl_single", on-click="">
+        <button class="btn">Kawowy Kalkulator</button>
+      </td>
+    </tr>
+    <tr>
+      <td class="panel_tbl_single">
+        <button class="btn">Z czym to się pije... czyli kawowe ABC</button>
+      </td>
+    </tr>
+   
+
+  </table>
+		</td>
+		<td>
+
+<p>
+  <h1>Zamówienie kawy</h1>
+  <h3>Gatunki kawy (Kawa ziarnista)</h3>
+  
+<?php
+    function calculate($num1, $num2, $operator) {
+        switch($operator) {
+            case '+':
+                $result = $num1 + $num2;
+                break;
+            case '-':
+                $result = $num1 - $num2;
+                break;
+            case '*':
+                $result = $num1 * $num2;
+                break;
+            case '/':
+                $result = $num1 / $num2;
+                break;
+            default:
+                $result = 'Invalid operator';
+        }
+
+        return $result;
+}
+
+echo calculate(5, 10, '+'); // 15
+echo calculate(5, 10, '-'); // 5
+echo calculate(5, 10, '*'); // 50
+echo calculate(5, 10, '/'); // 0.5
+echo calculate(5, 10, 'a'); // Invalid operator
+?>
+</p>
+
+		</td>
+  </tr>
+  <tr>
+    <td colspan="2" height="50" class="footer">Copyright © 2023 All Rights Reserved</td>
+  </tr>
+
+</table>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,19 @@
+create PHP website image gallery
+
+<?php 
+$imageFolder = 'images';
+$images = glob("$imageFolder/*.{jpg,gif,png,jpeg}", GLOB_BRACE);
+?>
+
+<div class="gallery">
+<?php
+foreach($images as $image) {
+	echo "<div class=\"thumbnail\">";
+	echo "<a href=\"$image\">";
+	echo "<img src=\"$image\" width=\"300\"/>";
+	echo "</a>";
+	echo "</div>";
+}
+?>
+
+</div>
