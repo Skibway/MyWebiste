359127a88f8415ba8cca566bea4cd333b5b62815
Skibway <86574686+Skibway@users.noreply.github.com>
2023-01-31 23:50:37 +0100

    Corrected data providing in order

--- order.php
+++ order.php
@@ -2,13 +2,13 @@
 <h3>Gatunki kawy (Kawa ziarnista)</h3>
 <form action="index.php?id=5" method="post">
   Arabica (14.69 PLN/100g)
-  <input type="text" name="arabica" class="input_elem"/> 1szt = 100g
+  <input type="text" name="arabica" class="input_elem" value="0"/> 1szt = 100g
   <br/> <br/>
   Robusta (11.70 PLN/100g)
-  <input type="text" name="robusta" class="input_elem"/> 1szt = 100g
+  <input type="text" name="robusta" class="input_elem" value="0"/> 1szt = 100g
   <br/> <br/>
   Mieszane 50/50 (13.20 PLN/100g)
-  <input type="text" name="mix" class="input_elem"/> 1szt = 100g
+  <input type="text" name="mix" class="input_elem" value="0"/> 1szt = 100g
   <br/> <br/>
   <input type="submit" value="zamawiam" class="btn_form"/>
 </form>
